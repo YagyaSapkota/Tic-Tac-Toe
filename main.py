0de4acbd3ceb47c4546b12153ad00121d0bf8c0b
Er Yagya Sapkota <officialyagya26@gmail.com>
2024-05-25 21:26:30 +0545

    Done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Python Program to play Tic Tac Toe Game
+def sum(p,q,r):
+    return p + q +r
 def printBoard(xState,zState):
     zero = 'X' if xState[0] else ('O' if zState[0] else 0)
     one = 'X' if xState[1] else ('O' if zState[1] else 1)
@@ -16,22 +18,41 @@ def printBoard(xState,zState):
     print(f"{six} | {seven} | {eight}")
     print(f"---------")   
 def checkwinner(xState,zState):
-    xwins 
-    if(sum(i) == 3):
-        return 1 
+    wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for win in wins:
+     if(sum(xState[win[0]] ,xState[win[1]] ,xState[win[2]]) == 3):
+         print("X won the Match")
+         return 1 
+     if(sum(zState[win[0]] ,zState[win[1]] ,zState[win[2]]) == 3):
+        print("O won the Match")
+        return 0
+    return -1
 if __name__=="__main__":
     xState = [0,0,0,0,0,0,0,0,0]
     zState =[0,0,0,0,0,0,0,0,0]
-    turn = 1 # 1 for X and 0 for O
+    turn = 1
     print("Welcome To Tic Tac Toe")
     while(True):
        printBoard(xState,zState)
-       if(turn==1):
+       if(turn == 1):
                print("Here,X's Chance dude ")
                value = int( input("Please enter a Value : "))
-               xState[value] = 1
-       else:  
-              print("Here,O's Chance Dude")
-              value= int(input("Please enter a value : "))
-              zState[value] = 1
-turn = 1- turn
+               xState[value] = 1 
+       else: 
+         print("Here,O's Chance Dude")
+         value= int(input("Please enter a value : "))
+         zState[value] = 1
+       CWIN= checkwinner(xState,zState)
+       if(CWIN != -1):
+        print("Match Over")
+        break
+              
+       turn = 1 - turn
+              
+        
+     
+              
+
+       
+            
+            
